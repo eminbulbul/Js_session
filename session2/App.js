a515aad7e5aa58555f9b3f41925a5e751fcbe565
m_emin <mehmeteminbul@gmail.com>
2022-01-20 23:13:32 +0300

    let and const

--- session2/App.js
+++ session2/App.js
@@ -1,9 +1,39 @@
 // console.log("I'm from file");
 
-var x = 7;
-var y = 5;
+// var x = 7;
+// var y = 5;
 
-console.log(x);
-console.log({ y });
-y = x
-console.log(y);
+// console.log(x);
+// console.log({ y });
+// y = x
+// console.log(y);
+
+// var alert = 'Merhaba'
+// console.log(alert);
+// alert('Hey')
+
+// var a = 'Global'
+// console.log(a); {
+//     let a = 'Scope'
+//     console.log(a); {
+//         let a = 'inner scope'
+//         console.log(a);
+
+
+//     }
+//     console.log(a);
+//     // eğer inner scope içerisinde let ifadesi kullanılmasaydı, yukarıdaki console.log(a); değişikliği farklı olurdu. scope olmazdı
+
+// }
+// console.log(a);
+
+// let c = 3;
+// console.log(c);
+// c = 5;
+// console.log(c);
+
+const h = 'const var'
+console.log(h);
+const h = '2'
+
+//daha önce declare edildiği için const'a ikinci bir sefer daha atama yapılmaz
