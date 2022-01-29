f7db215c3eb09760fd56ff42fb83ff7e67f1f77a
m_emin <mehmeteminbul@gmail.com>
2022-01-29 12:51:35 +0300

    conditions updated

--- loops/App.js
+++ loops/App.js
@@ -48,16 +48,56 @@
 
 //for
 
-let i = 0;
+// let i = 0;
 
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-    i++
-}
-console.log(i);
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+//     i++
+// }
+// console.log(i);
+
+// -----------------------------------------------------------------
 
 // for (; i < 10; i++) {    i disarda tanimlanirsa sanki icinde tanimlanmis gibi basina ; koyulabilir. 
 //     console.log(i);
 //     i++
 // }
 // console.log(i);
+
+// let str = 'clarusway'
+
+// console.log(str.length);
+
+// console.log(str[str.length-1]);  //son elemana ulasmak icin kullanilir.
+
+// console.log(str[0]);
+
+// for (let i = 0; i < str.length; i++) {
+//     console.log({ i }, str[i]);
+// }
+
+// ----------------continue ve break ornekleri ----------------
+
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 5 == 0) continue; //5 in katlarini yazdirmadik
+//     console.log(i);
+// }
+
+// for (let a = 0; a <= 100; a++) {
+//     if (a % 5 == 0) break; //5 in ilk katinda donguyu bozar
+//     console.log(i);
+// }
+
+while (true) {
+    let x = prompt(`bir sayi giriniz: \n q ile cikis yapiniz`);
+    if (x == `q`) {
+        console.log(`cikis yapildi`);
+        break;
+    } else if (isNaN(x)) {
+        continue;
+    } else {
+        console.log(`${x}'in karesi = ${x*x}`);
+        break; //buraya break yazarak ilk dogru islemden sonra koddan cikariz
+    }
+}
+// else icindeki break silinirse q yazana kadar yazilan sayilarin karesi alan bir kod olur
