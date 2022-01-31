1a35292f9482d65a0a568ca3e56f6c05baf9bf90
m_emin <mehmeteminbul@gmail.com>
2022-02-01 00:02:24 +0300

    string methods begins

---
+++ string methods/App.js
@@ -0,0 +1,20 @@
+// let str1 = 'hello'
+// let str2 = "World"
+// let str3 = `${str1 + str2}`
+
+// console.log(typeof str1, str1);
+// console.log(typeof str2, str2);
+// console.log(typeof str3, str3);
+
+// let str4 = new String('A string object')
+// console.log(typeof str4, str4);
+//bu bir obje, primitive degil
+
+//concat()
+
+// let s1 = 'hello'
+// let s2 = 'world'
+// let s3 = s1.concat(s2);
+// console.log(s3);
+
+//charAt()
