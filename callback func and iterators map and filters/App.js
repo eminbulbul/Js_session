1cf6273c0d51e5f45b9dda094513f610012c2f3c
m_emin <mehmeteminbul@gmail.com>
2022-03-03 22:37:26 +0300

    call back func ended

--- callback func and iterators map and filters/App.js
+++ callback func and iterators map and filters/App.js
@@ -3,7 +3,7 @@
 //* ======================================================
 
 //* A callback function is a function that is passed as an argument
-//* to another function, to be "called back" at a later time.
+//* to another function, to be “called back” at a later time.
 //* A function that accepts other functions as arguments is called
 //* a higher-order function.
 
@@ -16,16 +16,58 @@
 //! 1- Event handlers are a good example of a type of callback function.
 //!--------------------------------------------------------------------
 
-// const increment = () =>{
-//  const counter = document.querySelector('#counter')
-//  counter.textContent = Number(counter.textContent) + 1;
-// };
+// const increment = () => {
+//    const counter = document.querySelector("#counter")
+//    counter.textContent = Number(counter.textContent) + 1;
+// }
 
-// document.querySelector('button').addEventListener('click',increment);
-
-document.querySelector('button').addEventListener('click',()=>{
-    const counter = document.querySelector('#counter')
+document.querySelector("button").addEventListener("click", () => {
+    const counter = document.querySelector("#counter");
     counter.textContent = Number(counter.textContent) + 1;
-});
-
-// increment() we acces func from here. we dont want this
+  });
+  
+  // increment()
+  
+  //! 2- Efective Programming Approach
+  //!------------------------------------------------------------
+  
+  const sum = (a, b) => a + b;
+  const sub = (a, b) => a - b;
+  const mul = (a, b) => a * b;
+  const div = (a, b) => a / b;
+  
+  const calculate = (callback) => {
+    const num1 = Number(prompt("First Number"));
+    const num2 = Number(prompt("Second Number"));
+    console.log("RESULT:", callback(num1, num2));
+  };
+  
+  calculate(sum);
+  calculate(sub);
+  
+  //! 3- Async Programming
+  //!------------------------------------------------------------
+  const showLoadingScreen = () => {
+    console.log("1- Loading Screen appeared");
+  };
+  
+  const getDataFromDatabase = (callback) => {
+    console.log("2- Starting Getting Data");
+  
+    //! setTimeout is a non blocking-code,it runs in background
+    setTimeout(() => {
+      console.log("3- Finished Getting Data");
+      callback();
+      hideLoadingScreen();
+    }, 1000);
+  };
+  
+  const hideLoadingScreen = () => {
+    console.log("4- Loading Screen Hiding");
+  };
+  
+  showLoadingScreen();
+  getDataFromDatabase();
+  
+  // getDataFromDatabase(hideLoadingScreen);
+  getDataFromDatabase(() => console.log("4- Loading Screen Hiding"));
